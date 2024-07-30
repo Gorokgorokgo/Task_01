f7db91d3bfca6e4d653edbebeace5a5628170cff
박용준 <dydwnsl02@naver.com>
2024-07-31 02:45:48 +0900

    Level 1 - 7

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,18 +6,18 @@ public class App {
     public static void main(String[] args) {
         Boolean endFlag = true;
         // 반복 실행을 멈추기위한 flag
-        double[] resultArr = new double[10];
+        Stack<Double> resultStack = new Stack<Double>();
         int count = 0;
         while (endFlag) {
 
             Scanner sc = new Scanner(System.in);
 
             System.out.print("첫 번째 숫자를 입력하세요: ");
-            double num1 = (double)sc.nextInt();
+            double num1 = (double) sc.nextInt();
             // sc 입력변수로 받은 int 타입의 수를 num1로 저장
 
             System.out.print("두 번째 숫자를 입력하세요: ");
-            double num2 = (double)sc.nextInt();
+            double num2 = (double) sc.nextInt();
             // sc 입력변수로 받은 int 타입의 수를 num2로 저장
 
             char sign = ' ';
@@ -54,28 +54,27 @@ public class App {
             }
             if (num2 == 0 && sign == '/') {
                 System.out.print("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. \n\n");
-            } else if (count == 10) {
-                for (int i = 0; i < count - 1; i++) { // 10개의 배열
-                    resultArr[i] = resultArr[i + 1]; // 인덱스 위치 한 칸씩 왼쪽 덮어쓰기
-                }
-                resultArr[count - 1] = result; // 마지막 인덱스는 마지막 결과 값
-                System.out.print("num1 " + sign + " num2 = " + result + "\n\n"); // 결과값 출력
-
             } else {
                 System.out.print("num1 " + sign + " num2 = " + result + "\n\n"); // 결과값 출력
-                resultArr[count] = result;
-                count++;
+                resultStack.push(result); // Stack에 result 값 저장
+
+                System.out.print("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                String remove = sc.next(); // scanner로 받아온 sc 값중 한 단어만 remove 변수로 문자열 저장
+                if (remove.equalsIgnoreCase("remove")) {
+                    resultStack.pop();
+                }
             }
 
             System.out.print("더 계산하시려면 아무 값이나 입력하세요. (exit 입력 시 종료)");
-            Scanner endSc = new Scanner(System.in);
-            // 사용자 입력 값을 endSc 변수로 저장
-            String exit = endSc.next();
-            // endSc 변수를 .next() 메서드를 이용하여 한 단어만 exit 변수로 문자열 저장
+            String exit = sc.next();
+            // sc 변수를 .next() 메서드를 이용하여 한 단어만 exit 변수로 문자열 저장
             if (exit.equalsIgnoreCase("exit")) {
                 endFlag = false; // 전체 반복 해제
             }
         }
-        System.out.println(Arrays.toString(resultArr));
+        while(!resultStack.isEmpty()) {
+            System.out.print(resultStack.pop() + " "); // 결과 값 체크
+        }
+        // System.out.println(Arrays.toString(resultArr)); // 결과 값 체크
     }
 }
